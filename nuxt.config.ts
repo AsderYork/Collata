2fcfa527b44b54294d7652b8f53cd1c97c54f333
d.mosin <d.mosin@stroyservis.com>
2023-09-25 17:02:27 +0700

    AuthCheck is 10-minutes now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
         enableRefreshOnWindowFocus: true,
   
         // Whether to refresh the session every `X` milliseconds. Set this to `false` to turn it off. The session will only be refreshed if a session already exists.
-        enableRefreshPeriodically: 5000
+        enableRefreshPeriodically: 1000*60*10
       },
     globalAppMiddleware: {
       isEnabled: true
